beace34fc665a01edfb0d22a776490818ec00fcc
adanfrancisco <adanfrancisco@gmail.com>
2025-03-14 10:00:04 -0300

    aa

--- src/pages/temario/Fiche.jsx
+++ src/pages/temario/Fiche.jsx
@@ -2,11 +2,11 @@ import userStore from "../../stores/userStore";
 import styles from "./Temario.module.css";
 
 const Fiche = () => {
-    const { apellido, nombres, dni } = userStore(state => state);
+  const { apellido, nombres, dni } = userStore(state => state);
   return (
     <div>
 
-        <div className={styles.container}>
+      <div className={styles.container}>
         <h2>Información del Usuario</h2>
         <div className={styles.card}>
           <p>
@@ -21,7 +21,7 @@ const Fiche = () => {
           {/* <p>
             TIPO: {typeUser}
           </p> */}
-
+          <p>Dirijase a su materia por favor, tanto para escribir en el temario como para verificar que se guardó su clase</p>
         </div>
       </div>
 
