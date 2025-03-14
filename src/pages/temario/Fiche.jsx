221b9dc3fcea2c6c1dffa47231ac87c17a2df74c
adanfrancisco <adanfrancisco@gmail.com>
2025-03-14 10:06:35 -0300

    a

--- src/pages/temario/Fiche.jsx
+++ src/pages/temario/Fiche.jsx
@@ -18,10 +18,9 @@ const Fiche = () => {
           <p>
             DNI: {dni}
           </p>
-          {/* <p>
-            TIPO: {typeUser}
-          </p> */}
-          <p>Dirijase a su materia por favor, tanto para escribir en el temario como para verificar que se guardó su clase</p>
+          <p>Dirijase a su materia por favor, tanto para escribir en el temario como para verificar que se guardó su clase
+
+          </p>
         </div>
       </div>
 
